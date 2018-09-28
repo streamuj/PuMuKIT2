e945eb096d66c9fa1007e92af42a33e316dffb49
Alfonso Rodríguez <arodriguez@teltek.es>
2018-09-28 18:38:43 +0200

    TTK-19189 WebTVBundle: Refactored ChannelController
    Separated logic functions unto a service

--- src/Pumukit/WebTVBundle/Controller/ChannelController.php
+++ src/Pumukit/WebTVBundle/Controller/ChannelController.php
@@ -9,26 +9,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class ChannelController extends Controller implements WebTVController
 {
-    private $titles = array(
-        1 => 'University',
-        2 => 'Business',
-        3 => 'Natural Sciences',
-        5 => 'Humanities',
-        6 => 'Health & Medicine',
-        4 => 'Law',
-        7 => 'Social Matters & Education',
-    );
-
-    private $tags = array(
-        1 => array('PUDEUNI'), //"University",
-        2 => array('100'), //"Business",
-        3 => array('101', '108', '109'), //"Natural Sciences",
-        5 => array('102', '104', '106', '107', '114', '115'), //"Humanities",
-        6 => array('103'), //"Health & Medicine",
-        4 => array('116'), //"Law",
-        7 => array('110', '111', '112', '113'), //"Social Matters & Education",
-    );
-
     /**
      * @Route("/series/channel/{channelNumber}.html", name="pumukit_webtv_channel_series")
      * @Template("PumukitWebTVBundle:Channel:index.html.twig")
@@ -41,22 +21,11 @@ class ChannelController extends Controller implements WebTVController
         $repoSeries = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:Series');
         $repoMmobj = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:MultimediaObject');
 
-        $channelTitle = $this->getChannelTitle($channelNumber);
-        $channelTags = $this->getTagsForChannel($channelNumber);
-        $results = array();
+        $channelService = $this->get('pumukit_web_tv.channels');
+        $channelTitle = $channelService->getChannelTitle($channelNumber);
+        $channelTags = $channelService->getTagsForChannel($channelNumber);
 
-        foreach ($channelTags as $tag) {
-            $series = $repoSeries->createBuilderWithTag($tag, array('record_date' => -1));
-            $series = $series->getQuery()->execute();
-            $numMmobjs = $repoMmobj->createBuilderWithTag($tag, array('record_date' => -1))
-                                   ->count()->getQuery()->execute();
-
-            $results[] = array(
-                'tag' => $tag,
-                'objects' => $series,
-                'numMmobjs' => $numMmobjs,
-            );
-        }
+        $results = $channelService->getChannelSeriesByTags($channelTags);
 
         $this->updateBreadcrumbs($channelTitle, 'pumukit_webtv_channel_series', array('channelNumber' => $channelNumber));
 
@@ -65,26 +34,6 @@ class ChannelController extends Controller implements WebTVController
             'results' => $results,
             'number_cols' => $numberCols,
         );
-    }
-
-    public function getChannelTitle($channelNumber)
-    {
-        $title = isset($this->titles[$channelNumber]) ? $this->titles[$channelNumber] : 'No title';
-        $title = $this->get('translator')->trans($title);
-
-        return $title;
-    }
-
-    public function getTagsForChannel($channelNumber)
-    {
-        $tagCods = isset($this->tags[$channelNumber]) ? $this->tags[$channelNumber] : array();
-        $tags = array();
-        $repoTags = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:Tag');
-        foreach ($tagCods as $tagCod) {
-            $tags[] = $repoTags->findOneByCod($tagCod);
-        }
-
-        return $tags;
     }
 
     private function updateBreadcrumbs($title, $routeName, array $routeParameters = array())
